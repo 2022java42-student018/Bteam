71d7ba18c364ed9f8d9b4f26adb91dcaa973dc3a
WIN-VOCI3DI5QBM\student <2022java42-student018@la-study.com>
2022-05-24 17:21:01 +0900

    進捗（西島）

---
+++ src/main/java/la/LoginServlet.java
@@ -0,0 +1,32 @@
+package la;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.dao.DAOException;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			
+		}catch(DAOException e) {
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
