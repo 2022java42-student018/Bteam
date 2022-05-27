f2c502e3c5f11c37d34ebbaf098237d916fd7387
WIN-VOCI3DI5QBM\student <2022java42-student018@la-study.com>
2022-05-27 16:21:09 +0900

    途中進捗（西島）

--- src/main/java/la/dao/RentalDAO.java
+++ src/main/java/la/dao/RentalDAO.java
@@ -29,7 +29,6 @@ public class RentalDAO {
 	public boolean IDCheck(int dID) throws DAOException {
 		// SQL文の作成
 		String sql = "SELECT renCID FROM item WHERE dID= ?";
-		boolean Check = false;
 
 		try (// データベースへの接続
 				Connection con = DriverManager.getConnection(url, user, pass);
@@ -40,14 +39,16 @@ public class RentalDAO {
 			try (
 					/// SQLの実行
 					ResultSet rs = st.executeQuery();) {
+				boolean Check = false;
 				if (rs.next()) {
 					Check = true;
 				}
+				return Check;
 			} catch (SQLException e) {
 				e.printStackTrace();
 				throw new DAOException("会員IDエラー");
 			}
-			return Check;
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DAOException("会員IDエラー");
@@ -58,7 +59,7 @@ public class RentalDAO {
 	public boolean fivebooks(int cID) throws DAOException {
 		// SQL文の作成
 		String sql = "SELECT renCID FROM item WHERE renCID =?";
-		boolean Check = true;
+
 		try (// データベースへの接続
 				Connection con = DriverManager.getConnection(url, user, pass);
 				// PreparedStatementオブジェクトの取得
@@ -67,12 +68,14 @@ public class RentalDAO {
 			st.setInt(1, cID);
 			try (// SQLの実行
 					ResultSet rs = st.executeQuery();) {
+				boolean Check = true;
 				rs.last();
 				int renDocument = rs.getRow();
-				
-				if(renDocument >= 5) {
+
+				if (renDocument >= 5) {
 					Check = false;
 				}
+				return Check;
 			} catch (SQLException e) {
 				e.printStackTrace();
 				throw new DAOException("会員IDエラー");
@@ -81,13 +84,13 @@ public class RentalDAO {
 			e.printStackTrace();
 			throw new DAOException("会員IDエラー");
 		}
-	return Check;
+
 	}
-	
+
 	public boolean overrent(int cID) throws DAOException {
 		// SQL文の作成
 		String sql = "SELECT retDate FROM item WHERE renCID =?";
-		boolean Check = true;
+
 		try (// データベースへの接続
 				Connection con = DriverManager.getConnection(url, user, pass);
 				// PreparedStatementオブジェクトの取得
@@ -96,19 +99,21 @@ public class RentalDAO {
 			st.setInt(1, cID);
 			try (// SQLの実行
 					ResultSet rs = st.executeQuery();) {
+				boolean Check = true;
 				Date retDate = rs.getDate("retDate");
-				Calendar retCalendar =  Calendar.getInstance();
-				Calendar today =  Calendar.getInstance();
-				
+				Calendar retCalendar = Calendar.getInstance();
+				Calendar today = Calendar.getInstance();
+
 				retCalendar.setTime(retDate);
-				
+
 				int year = retCalendar.get(Calendar.YEAR);
 				int month = retCalendar.get(Calendar.MONTH);
 				int date = retCalendar.get(Calendar.DATE);
-				
-				retCalendar.set(year,month,date,0,0,0);
-				
+
+				retCalendar.set(year, month, date, 0, 0, 0);
+
 				Check = retCalendar.before(today);
+				return Check;
 			} catch (SQLException e) {
 				e.printStackTrace();
 				throw new DAOException("会員IDエラー");
@@ -117,38 +122,11 @@ public class RentalDAO {
 			e.printStackTrace();
 			throw new DAOException("会員IDエラー");
 		}
-	return Check;
 	}
-		
+
 	public String getdName(int dID) throws DAOException {
 		// SQL文の作成
 		String sql = "SELECT dName FROM item WHERE dID =?";
-		String dName = null;
-		try (// データベースへの接続
-				Connection con = DriverManager.getConnection(url, user, pass);
-				// PreparedStatementオブジェクトの取得
-				PreparedStatement st = con.prepareStatement(sql);) {
-			// プレースホルダ
-			st.setInt(1, dID);
-			try (// SQLの実行
-					ResultSet rs = st.executeQuery();) {
-				dName = rs.getString(dName);
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	return dName;
-	}
-	
-	public Calendar getretlineDay(int dID) throws DAOException {
-		// SQL文の作成
-		String sql = "SELECT aDate FROM item WHERE dID =?";
-		Calendar today = Calendar.getInstance();
-		Calendar retlineDay = Calendar.getInstance();
-		Calendar aCalendar = Calendar.getInstance();
 		
 		try (// データベースへの接続
 				Connection con = DriverManager.getConnection(url, user, pass);
@@ -156,33 +134,71 @@ public class RentalDAO {
 				PreparedStatement st = con.prepareStatement(sql);) {
 			// プレースホルダ
 			st.setInt(1, dID);
+
 			try (// SQLの実行
 					ResultSet rs = st.executeQuery();) {
-				Date aDate = rs.getDate("aDate");
-				aCalendar.setTime(aDate);
-				
-				int year = today.YEAR;
-				int month = today.MONTH;
-				int date = today.DATE;
-				
-				int a_year = aCalendar.YEAR;
-				int a_month = aCalendar.MONTH+3;
-				int a_date = aCalendar.DATE;
-				
-				today.set(year,month,date,0,0,0);
-				aCalendar.set(a_year,a_month,a_date,0,0,0);
-				
-				if(aCalendar.before(today)) {
-					
+				String dName = null;
+				if (rs.next()) {
+					dName = rs.getString("dName");
 				}
-				
+				return dName;
 			} catch (SQLException e) {
 				e.printStackTrace();
+				throw new DAOException("資料IDエラー");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+			throw new DAOException("資料IDエラー");
 		}
-	return retlineDay;
+	}
+
+	public Calendar getretlineDay(int dID) throws DAOException {
+		// SQL文の作成
+		String sql = "SELECT aDate FROM item WHERE dID =?";
+
+		try (// データベースへの接続
+				Connection con = DriverManager.getConnection(url, user, pass);
+				// PreparedStatementオブジェクトの取得
+				PreparedStatement st = con.prepareStatement(sql);) {
+			// プレースホルダ
+			st.setInt(1, dID);
+			try (// SQLの実行
+					ResultSet rs = st.executeQuery();) {
+
+				Calendar today = Calendar.getInstance();
+				Calendar retlineDay = Calendar.getInstance();
+				Calendar aCalendar = Calendar.getInstance();
+
+				Date aDate = rs.getDate("aDate");
+				aCalendar.setTime(aDate);
+				aCalendar.add(aCalendar.MONTH, 3);
+
+				int year = today.YEAR;
+				int month = today.MONTH;
+				int date = today.DATE;
+
+				int a_year = aCalendar.YEAR;
+				int a_month = aCalendar.MONTH;
+				int a_date = aCalendar.DATE;
+
+				today.set(year, month, date, 0, 0, 0);
+				retlineDay.set(year, month, date, 0, 0, 0);
+				aCalendar.set(a_year, a_month, a_date, 0, 0, 0);
+
+				if (aCalendar.after(today)) {
+					retlineDay.add(retlineDay.DATE, 10);
+				} else {
+					retlineDay.add(retlineDay.DATE, 15);
+				}
+				return retlineDay;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("資料IDエラー");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("資料IDエラー");
+		}
+
 	}
 }
-
