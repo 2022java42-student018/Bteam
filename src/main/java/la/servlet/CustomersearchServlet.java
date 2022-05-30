a5512f1e7c9368753bb3dafc069a40aafd9d5b58
WIN-L8KPO2CA32R\student <2022java42-student012@la-study.com>
2022-05-30 14:06:41 +0900

    本日の進捗です（瑞慶覧）

--- src/main/java/la/servlet/CustomersearchServlet.java
+++ src/main/java/la/servlet/CustomersearchServlet.java
@@ -1,6 +1,7 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -13,82 +14,87 @@ import javax.servlet.http.HttpSession;
 
 import la.bean.CustomerBean;
 import la.bean.historyBean;
-import la.bean.lend_docBean;
 import la.dao.CustomerDAO;
 import la.dao.DAOException;
-
 
 @WebServlet("/CustomersearchServlet")
 
 public class CustomersearchServlet extends HttpServlet {
 
-	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    try {
-        request.setCharacterEncoding("UTF-8");
-        String action = request.getParameter("action");
-		 CustomerDAO dao = new CustomerDAO();
-		 
-		HttpSession session = request.getSession(); 
-         
-		if(action.equals("eMailSearch")) {
-			
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			request.setCharacterEncoding("UTF-8");
+			String action = request.getParameter("action");
+			CustomerDAO dao = new CustomerDAO();
 
-			 if (session != null) { // セッションオブジェクトなし
-			 session.removeAttribute("Customerdata");
-			 }
-			 
-          String eMail =request.getParameter("email");
-        List<CustomerBean> list= dao.Emailcheck(eMail);
-        
-        if(list != null) {
-        	String eMail1 =request.getParameter("email");
-            List<CustomerBean> list1= dao.emailSearch(eMail1);	
-            request.setAttribute("customer", list1);
-            gotoPage(request,response,"/showcustomer.jsp");//showcustomerﾍ
-        
-        }else
-        	request.setAttribute("message", "Emailが間違っています");
+			HttpSession session = request.getSession();
+
+			if (action.equals("eMailSearch")) {
+				String eMail = request.getParameter("email");
+				session.setAttribute("email", eMail);
+				
+				if (eMail == "") {
+					request.setAttribute("message", "Emailが未入力です");
+					gotoPage(request, response, "/customerError.jsp");
+				}
+				
+				List<CustomerBean> list = dao.Emailcheck(eMail);
+				request.setAttribute("customer", list);
+				// System.out.println(list);
+
+				if (list.size() == 0) {
+					request.setAttribute("message", "Emailが間違っています");
+					gotoPage(request, response, "/customerError.jsp");
+				} else {
+					gotoPage(request, response, "/showcustomer.jsp");// showcustomerﾍ
+				}
+
+			} else if (action.equals("rend_doc")) {
+				ArrayList<String> inemail = (ArrayList<String>)session.getAttribute("customer",cID);
+				
+				//request.getAttribute("customer");
+				session.setAttribute ("document",inmail);
+//				String cID = ${inemai.cID};
+//				String eMail1 = (String) session.getAttribute("email");
+//				
+//				
+//				List<lend_docBean> list= dao.rend_doc(eMail1);
+//				request.setAttribute("document" list);
+				
+				//int cID = Integer.parseInt(request.getParameter("cID"));
+				//List<lend_docBean> list = dao.rend_doc(cID);
+
+				//request.setAttribute("document", list);
+
+				System.out.println(session.getAttribute("email"));
+				gotoPage(request, response, "/lend_doc.jsp");// lend_doc.jspﾍ
+
+			}
+
+			else if (action.equals("history_doc")) {
+
+				int cID = Integer.parseInt(request.getParameter("cID"));
+
+				List<historyBean> list = dao.history_doc(cID);
+
+				request.setAttribute("history", list);
+				gotoPage(request, response, "/history_doc.jsp");
+			}
+		} catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "入力した内容に不備があります");
 			gotoPage(request, response, "/customerError.jsp");
-
-        
-    
-			
-         }else if (action.equals("rend_doc")) {
-        	 
-             int cID = Integer.parseInt(request.getParameter("cID"));
-             List<lend_docBean> list = dao.rend_doc(cID);
-              
-             request.setAttribute("document",list);
-
-             gotoPage(request, response, "/lend_doc.jsp");// lend_doc.jspﾍ
-           
-           }
-    
-         else if (action.equals("history_doc")){
-			
-			int cID = Integer.parseInt(request.getParameter("cID"));
-			
-			 List<historyBean> list = dao.history_doc(cID);
-			
-			 request.setAttribute("history",list);
-			 gotoPage(request, response, "/history_doc.jsp");
-           }
-           }catch(DAOException e) {
-		e.printStackTrace();
-		request.setAttribute("message", "入力した内容に不備があります");
-		gotoPage(request,response,"/customerError.jsp");
+		}
 	}
-    }  
-   
-  
-    private void gotoPage(HttpServletRequest request, HttpServletResponse response, String page)
-            throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher(page);
-        rd.forward(request, response);
-    }
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+
+	private void gotoPage(HttpServletRequest request, HttpServletResponse response, String page)
+			throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		doGet(request, response);
--- src/main/webapp/showcustomer.jsp
+++ src/main/webapp/showcustomer.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-	
+
 <!DOCTYPE html>
 
 <html>
@@ -12,30 +12,35 @@
 <body>
 	<table border="1">
 		<tr>
-			<th>会員ID</th><th>氏名</th><th>電話番号</th><th>Eメール</th><th>生年月日</th><th>入会年月日</th>
+			<th>会員ID</th>
+			<th>氏名</th>
+			<th>電話番号</th>
+			<th>Eメール</th>
+			<th>生年月日</th>
+			<th>入会年月日</th>
 		</tr>
 		<c:forEach items="${customer}" var="cust">
 			<tr>
-				<td>${cust.cID }</td><td>${cust.cName }</td><td>${cust.cTell }</td><td>${cust.cMail }</td><td>${cust.cBday }</td><td>${cust.cJdate }</td>
+				<td>${cust.cID }</td>
+				<td>${cust.cName }</td>
+				<td>${cust.cTell }</td>
+				<td>${cust.cMail }</td>
+				<td>${cust.cBday }</td>
+				<td>${cust.cJdate }</td>
 			</tr>
 		</c:forEach>
 	</table>
-	<!-- <a href="/Bteam/lend_doc.jsp">貸出中資料</a>
-	<input type="hidden" name="action" value="${cust.cID }"> -->
-	getparrmeter
+
+
+	<a href="/Bteam/CustomersearchServlet?action=rend_doc">貸出中資料</a>
 	
-	<form action="./CustomersearchServlet?" method="get">
-				<a href="/CustomersearchServlet?action=lend_doc">貸出中資料</a>
-				<!-- <input type="submit" value="rendoc"> -->
-				<input type="hidden" name="action" value="${cust.cID }">
-			</form>
-	
-	<a href="/Bteam/history_doc.jsp">貸出履歴</a> //historyテーブル
+
+	<a href="/Bteam/history_doc.jsp">貸出履歴</a>
 	<input type="hidden" name="action" value="${cust.cID }">
-	
-session.removeAttribute("Customerdata");
- 
-<a href ="/Bteam/documentSearch.jsp"><h2 align = "center">検索TOPへ</h2></a>
+
+
+
+	<a href="/Bteam/documentSearch.jsp"><h2 align="center">検索TOPへ</h2></a>
 
 
 </body>
